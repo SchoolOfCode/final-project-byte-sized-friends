d8adc87e2a9ea59de7ffcd3e5746faf830df845b
4rchie-r <archiereader@outlook.com>
2024-09-25 12:24:49 +0100

    changed the footer section to link rel imgs. Added some funky hover css too

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import './Footer.css'; 
+import './Footer.css';  // Include the CSS for styling
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} METIS.</p>
+      <div className="footer-icon">
+        <p className="material-symbols-outlined">public</p>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </div>
+      <div className="footer-icon">
+        <p className="material-symbols-outlined">chat_bubble</p>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </div>
+      <div className="footer-icon">
+        <p className="material-symbols-outlined">person</p>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </div>
     </footer>
   );
 };
