cf9776b9c30fcfd786956137df4014ef41051da7
4rchie-r <archiereader@outlook.com>
2024-09-25 09:56:38 +0100

    changed to v2.3.0

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import "./globals.css";
-import Home from "./app/page.js";
 
 export const metadata = {
   title: "METIS",
@@ -8,8 +7,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
-    <Home />
-    </>
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   );
 }
