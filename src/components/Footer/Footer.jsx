008135e42bf9e2af354bb6ab2b508bce4bf2a3b5
4rchie-r <archiereader@outlook.com>
2024-09-25 12:26:05 +0100

    unsure of whats happened

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import './Footer.css';  // Include the CSS for styling
+import React from "react";
+import "./Footer.css"; 
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-icon">
         <p className="material-symbols-outlined">public</p>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
       </div>
       <div className="footer-icon">
         <p className="material-symbols-outlined">chat_bubble</p>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
       </div>
       <div className="footer-icon">
         <p className="material-symbols-outlined">person</p>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
       </div>
     </footer>
   );
